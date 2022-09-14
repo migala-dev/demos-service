5bfd81520acb5e38e78428cdac1abaeac812ac1a
Alex Ortega <alexortegazj@gmail.com>
2022-09-13 18:35:18 -0700

    First commit

---
+++ src/auth/auth/auth.controller.spec.ts
@@ -0,0 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+describe('AuthController', () => {
+  let controller: AuthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [AuthService]
+    }).compile();
+
+    controller = module.get<AuthController>(AuthController);
+  });
+
+  describe('Login Call', () => {
+    it('should return the session with a correct phone number', () => {
+      
+    });
+  });
+  
+
+
+
+  
+});
