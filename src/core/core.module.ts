8c61432b7aaea138b8ddee074c544a5f343ac586
YaelBadillo <yael.badillos@gmail.com>
2022-10-02 22:56:06 -0500

    Fix: import UserDevice entity to the core module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -4,10 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './database/entities/user.entity';
 import { ConfigService } from '@nestjs/config';
 import { UserDeviceService } from './database/services/user-device.service';
+import { UserDevice } from './database/entities/user-device.entity';
 
 @Global()
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [TypeOrmModule.forFeature([User, UserDevice])],
     providers: [UsersService, ConfigService, UserDeviceService],
     exports: [UsersService, ConfigService, UserDeviceService]
 })
