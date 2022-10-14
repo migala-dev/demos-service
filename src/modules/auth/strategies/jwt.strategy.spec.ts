9556eb4708b52bb89da7dcb699d059bb4ee42213
YaelBadillo <yael.badillos@gmail.com>
2022-10-14 01:35:42 -0500

    Feat: add jwt strategy and its unit tests

---
+++ src/modules/auth/strategies/jwt.strategy.spec.ts
@@ -0,0 +1,125 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { createSpyObj } from 'jest-createspyobj';
+
+import { JwtStrategy } from './jwt.strategy';
+import { User } from '../../../core/database/entities/user.entity';
+import { UsersService } from '../../../core/database/services/user.service';
+
+describe('JwtStrategy', () => {
+  let strategy: JwtStrategy;
+  let usersSpyService: jest.Mocked<UsersService>;
+
+  beforeEach(async () => {
+    usersSpyService = createSpyObj(UsersService);
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [JwtStrategy, { provide: UsersService, useValue: usersSpyService }],
+    }).compile();
+
+    strategy = module.get<JwtStrategy>(JwtStrategy);
+  });
+
+  it('should be defined', () => {
+    expect(strategy).toBeDefined();
+  });
+
+  describe('validate method', () => {
+    let payload: { cognitoId: string };
+
+    beforeEach(() => {
+      payload = { cognitoId: '' };
+
+      usersSpyService.findOneByCognitoId.mockReturnValue((async () => ({}) as User)())
+    });
+
+    it('should be define', () => {
+      expect(strategy.validate).toBeDefined();
+    });
+
+    it('should be a function', () => {
+      expect(typeof strategy.validate).toBe('function');
+    });
+
+    it('should be an async function', () => {
+      const string: string = strategy.validate.toString();
+
+      const isAsync: boolean = string.includes('async');
+
+      expect(isAsync).toBeTruthy();
+    });
+
+    it('should be called with a payload argument as parameter', () => {
+      const validateSpyMethod: jest.SpyInstance = jest.spyOn(strategy, 'validate');
+
+      strategy.validate(payload);
+
+      expect(validateSpyMethod).toHaveBeenCalled();
+      expect(validateSpyMethod).toHaveBeenCalledWith(payload);
+    });
+
+    it('should return an object', async () => {
+      const result: object = await strategy.validate(payload);
+
+      expect(typeof result).toBe('object');
+    });
+
+    it('should return a User instance', async () => {
+      usersSpyService.findOneByCognitoId.mockReturnValue((async () => new User())())
+
+      const result: User = await strategy.validate(payload);
+
+      expect(result instanceof User).toBeTruthy();
+    });
+
+    it('should called findOneByCognitoId method from usersService property', async () => {
+      await strategy.validate(payload);
+
+      expect(usersSpyService.findOneByCognitoId).toHaveBeenCalled();
+    });
+
+    it('should throw if no user is found', async () => {
+      usersSpyService.findOneByCognitoId.mockReturnValue((async () => null)())
+
+      const execute = async () => await strategy.validate(payload);
+
+      await expect(execute).rejects.toThrow();
+    });
+
+    it('should throw UnauthorizedException error if no user is found', async () => {
+      usersSpyService.findOneByCognitoId.mockReturnValue((async () => null)())
+
+      const execute = async () => await strategy.validate(payload);
+
+      await expect(execute).rejects.toThrowError(UnauthorizedException);
+    });
+
+    it('should return same user returned by findOneByCognitoId method if user is found', async () => {
+      const expectedCognitoId: string = '';
+      const expectedUser: User = new User();
+      expectedUser.cognitoId = expectedCognitoId;
+      usersSpyService.findOneByCognitoId.mockReturnValue((async () => expectedUser)())
+
+      const result: User = await strategy.validate(payload);
+
+      expect(result).toEqual(
+        expect.objectContaining(expectedUser)
+      );
+    });
+
+    it('should return same user with the same cognitoId passed to the method if user is found', async () => {
+      const expectedCognitoId: string = 'Test';
+      payload = { cognitoId: expectedCognitoId }
+      const expectedUser: User = new User();
+      expectedUser.cognitoId = expectedCognitoId;
+      usersSpyService.findOneByCognitoId.mockReturnValue((async () => expectedUser)())
+
+      const result: User = await strategy.validate(payload);
+
+      expect(result).toEqual(
+        expect.objectContaining(expectedUser)
+      );
+    });
+  });
+});
--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,16 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+
 import { Strategy, ExtractJwt } from 'passport-jwt';
+
+import { User } from '../../../core/database/entities/user.entity';
+import { UsersService } from '../../../core/database/services/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly usersService: UsersService) {
     super({
+      secretOrKey: 'test',
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
 
-  async validate(payload) {
-    return {};
+  async validate(payload: { cognitoId: string }): Promise<User> {
+    const user: User = await this.usersService.findOneByCognitoId(payload.cognitoId);
+    if (!user) throw new UnauthorizedException();
+
+    return user;
   }
 }
