bc839aa089cdd50699f2ff207b923e283018fb1e
YaelBadillo <yael.badillos@gmail.com>
2022-10-16 22:59:40 -0500

    Refact: change secretOrKey to a pem JWK

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -2,16 +2,19 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 
 import { Strategy, ExtractJwt } from 'passport-jwt';
+import jwkToPem, { JWK } from 'jwk-to-pem';
 
 import { User } from '../../../core/database/entities/user.entity';
 import { UsersService } from '../../../core/database/services/user.service';
+import jwks from '../../../../jwks.json';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly usersService: UsersService) {
     super({
-      secretOrKey: 'test',
+      secretOrKey: jwkToPem(jwks.keys[1] as JWK),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
     });
   }
 
