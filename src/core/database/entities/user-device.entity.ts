77975c145efc19a680e8e501f12288923d3cd391
YaelBadillo <yael.badillos@gmail.com>
2022-10-18 22:28:08 -0500

    Refact: add name to UserDevices entity

--- src/core/database/entities/user-device.entity.ts
+++ src/core/database/entities/user-device.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity('user_devices')
 export class UserDevice {
   @PrimaryGeneratedColumn('uuid')
   userDeviceId: string;
