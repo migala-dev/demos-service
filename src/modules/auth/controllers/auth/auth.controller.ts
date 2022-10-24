4e1fb27df84fd5bd6590c9806433577791cdbaf3
YaelBadillo <yael.badillos@gmail.com>
2022-10-23 21:48:46 -0500

    Test: Refact tests

--- src/modules/auth/controllers/auth/auth.controller.ts
+++ src/modules/auth/controllers/auth/auth.controller.ts
@@ -10,22 +10,21 @@ import { Public } from '../../../../core/decorators/auth/public.decorator';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
 
-    constructor(private readonly authService: AuthService) {}
+  @Public()
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  public login(@Body() { phoneNumber }: LoginDto): Promise<LoginResponse> {
+    return this.authService.login(phoneNumber);
+  }
 
-    @Public()
-    @Post('login')
-    @HttpCode(HttpStatus.OK)
-    public login(@Body() { phoneNumber }: LoginDto): Promise<LoginResponse> {
-        return this.authService.login(phoneNumber);
-    }
-
-    @Post('user-device')
-    @HttpCode(HttpStatus.OK)
-    public registerUserDevice(
-      @Body() { deviceId }: UserDeviceDto, 
-      @UserFromRequest() { userId }: User,
-    ): Promise<UserDevice> {
-      return this.authService.registerUserDevice(userId, deviceId);
-    }
+  @Post('user-device')
+  @HttpCode(HttpStatus.OK)
+  public registerUserDevice(
+    @Body() { deviceId }: UserDeviceDto,
+    @UserFromRequest() { userId }: User,
+  ): Promise<UserDevice> {
+    return this.authService.registerUserDevice(userId, deviceId);
+  }
 }
