f2427dbaac635ca9a166b8b1d5dcd611e69a5127
YaelBadillo <yael.badillos@gmail.com>
2022-10-23 21:58:16 -0500

    Test: refact jwt-auth guard tests

--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -9,10 +9,12 @@ import { IS_PUBLIC_KEY } from '../../../core/decorators/auth/public.decorator';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   constructor(private reflector: Reflector) {
-    super()
+    super();
   }
 
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
