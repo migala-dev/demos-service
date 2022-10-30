d196f7d67068ef2a1f44437890f35c29a58aac60
YaelBadillo <yael.badillos@gmail.com>
2022-10-30 13:49:28 -0600

    Feat: add factories to generate entity mocks

---
+++ test/utils/entities-mock.factory.ts
@@ -0,0 +1,33 @@
+import { Space } from '../../src/core/database/entities/space.entity';
+import { Member } from '../../src/core/database/entities/member.entity';
+
+export const spaceMockFactory = async (chance: Chance.Chance) => {
+  const spaceMock: Space = new Space();
+  spaceMock.spaceId = chance.string();
+  spaceMock.name = chance.name();
+  spaceMock.description = chance.paragraph({ sentences: 1 });
+  spaceMock.pictureKey = chance.string();
+  spaceMock.approvalPercentage = chance.integer({ min: 51, max: 100 });
+  spaceMock.participationPercentage = chance.integer({ min: 51, max: 100 });
+  spaceMock.ownerId = chance.string();
+  spaceMock.createdAt = chance.date();
+  spaceMock.updatedAt = spaceMock.createdAt;
+
+  return spaceMock;
+};
+
+export const memberMockFactory = async (chance: Chance.Chance) => {
+  const memberMock: Member = new Member();
+  memberMock.memberId = chance.string();
+  memberMock.spaceId = chance.string();
+  memberMock.userId = chance.string();
+  memberMock.name = chance.name();
+  memberMock.expiredAt = chance.date();
+  memberMock.deleted = chance.bool();
+  memberMock.createdAt = chance.date();
+  memberMock.updatedAt = memberMock.createdAt;
+  memberMock.createdBy = chance.string();
+  memberMock.updatedBy = memberMock.createdBy;
+
+  return memberMock;
+};
