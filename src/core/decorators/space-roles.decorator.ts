3672cc22e1e2a31fea8f1e9c745c961555e2ee10
YaelBadillo <yael.badillos@gmail.com>
2022-10-31 01:19:31 -0600

    Refact: refact space roles guard.
    Guard to set metadata and apply space member after space roles guard.

--- src/core/decorators/space-roles.decorator.ts
+++ src/core/decorators/space-roles.decorator.ts
@@ -1,6 +1,12 @@
-import { SetMetadata } from '@nestjs/common';
+import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
 
 import { SpaceRole } from '../enums';
+import { SpaceMemberGuard } from '../guards/space-member/space-member.guard';
+import { SpaceRolesGuard } from '../guards/space-roles/space-roles.guard';
 
-export const SPACE_ROLES_KEY: string = 'space-roles';
-export const SpaceRoles = (...spaceRoles: SpaceRole[]) => SetMetadata(SPACE_ROLES_KEY, spaceRoles);
+export const SPACE_ROLES_KEY = 'space-roles';
+export const SpaceRoles = (...spaceRoles: SpaceRole[]) =>
+  applyDecorators(
+    SetMetadata(SPACE_ROLES_KEY, spaceRoles),
+    UseGuards(SpaceMemberGuard, SpaceRolesGuard),
+  );
