33dadc8b995b1f4f4ee932b057f0f5b29a4ec942
YaelBadillo <yael.badillos@gmail.com>
2022-10-30 21:15:58 -0600

    Test: add a new test to space member guard.

--- src/core/guards/space-member/space-member.guard.spec.ts
+++ src/core/guards/space-member/space-member.guard.spec.ts
@@ -14,6 +14,7 @@ import { MembersService } from '../../database/services/member.service';
 import { SpacesService } from '../../database/services/space.service';
 import { Member } from '../../database/entities/member.entity';
 import { Space } from '../../database/entities/space.entity';
+import { InternalServerErrorException } from '@nestjs/common';
 import {
   spaceMockFactory,
   memberMockFactory,
@@ -54,6 +55,15 @@ describe('SpaceMemberGuard', () => {
       spacesSpyService.findOneById.mockReturnValue(
         (async () => ({} as Space))(),
       );
+    });
+
+    it('should throw an InternalServerErrorException if there is no member in the request object', async () => {
+      requestObjectMock.user = undefined;
+      httpArgumentsHost.getRequest.mockReturnValue(requestObjectMock);
+
+      const execute = async () => await guard.canActivate(executionContextMock);
+
+      await expect(execute).rejects.toThrowError(InternalServerErrorException);
     });
 
     it('should throw a BadRequestException if space not found', async () => {
@@ -103,5 +113,53 @@ describe('SpaceMemberGuard', () => {
         expect.objectContaining(memberMock),
       );
     });
+    /*
+
+    it('should attach space and member owner to request object with undefined values', async () => {
+      const spaceMock: Space = new Space();
+      const memberMock: Member = new Member();
+      memberMock.spaceId = spaceMock.spaceId;
+      memberMock.userId = spaceMock.ownerId;
+      spacesSpyService.findOneById.mockReturnValue(
+        (async () => ({ ...spaceMock } as Space))(),
+      );
+      membersSpyService.findOneByUserIdAndSpaceId.mockReturnValue(
+        (async () => ({ ...memberMock } as Member))(),
+      );
+
+      await guard.canActivate(executionContextMock);
+
+      expect(requestObjectMock.space).toStrictEqual(
+        expect.objectContaining(spaceMock),
+      );
+      expect(requestObjectMock.member).toStrictEqual(
+        expect.objectContaining(memberMock),
+      );
+    });
+
+    it('should attach space and member owner to request object with manual data', async () => {
+      const spaceMock: Space = new Space();
+      spaceMock.name = 'aTestName';
+      spaceMock.description = 'aTestDescription';
+      const memberMock: Member = new Member();
+      memberMock.spaceId = spaceMock.spaceId;
+      memberMock.userId = spaceMock.ownerId;
+      spacesSpyService.findOneById.mockReturnValue(
+        (async () => ({ ...spaceMock } as Space))(),
+      );
+      membersSpyService.findOneByUserIdAndSpaceId.mockReturnValue(
+        (async () => ({ ...memberMock } as Member))(),
+      );
+
+      await guard.canActivate(executionContextMock);
+
+      expect(requestObjectMock.space).toStrictEqual(
+        expect.objectContaining(spaceMock),
+      );
+      expect(requestObjectMock.member).toStrictEqual(
+        expect.objectContaining(memberMock),
+      );
+    });
+    */
   });
 });
