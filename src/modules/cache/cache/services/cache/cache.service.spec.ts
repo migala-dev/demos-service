27e267b4a60f6967634a59e4a0555cc791b84e4e
Alex Ortega <alexortegazj@gmail.com>
2023-01-06 12:30:43 -0700

    Implementing new test on cache service

--- src/modules/cache/cache/services/cache/cache.service.spec.ts
+++ src/modules/cache/cache/services/cache/cache.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { createSpyObj } from 'jest-createspyobj';
+import { loginConstants } from '../../../../../../test/mocks/constants/constants';
 import { CacheRepository } from '../../../../../core/database/services/cache.repository';
 import { CacheService } from './cache.service';
 
@@ -10,13 +11,10 @@ describe('CacheService', () => {
   beforeEach(async () => {
     cacheSpyRepository = createSpyObj(CacheRepository);
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        CacheService,
-        {
-          provide: CacheRepository,
-          useValue: cacheSpyRepository,
-        },
-      ],
+      providers: [CacheService, {
+        provide: CacheRepository,
+        useValue: cacheSpyRepository,
+      }],
     }).compile();
 
     service = module.get<CacheService>(CacheService);
@@ -24,5 +22,17 @@ describe('CacheService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should get cache from cache repository', async () => {
+    const userId = loginConstants.userId;
+    const lastUpdatedDate: string = new Date().toDateString();
+      await service.getCache(userId, lastUpdatedDate);
+
+      expect(cacheSpyRepository.findAllByUserIdAfterDate).toHaveBeenCalledTimes(1);
+      expect(cacheSpyRepository.findAllByUserIdAfterDate).toHaveBeenCalledWith(
+        loginConstants.userId,
+        new Date(lastUpdatedDate),
+      );
   });
 });
